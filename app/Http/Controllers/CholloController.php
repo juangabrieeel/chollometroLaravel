1f3a09df97b0a493daa179929d73fccc79903a72
juangabrieeel <juangabrielcarodiaz16@gmail.com>
2024-02-25 14:07:21 +0100

    Controlador y configuración de rutas

---
+++ app/Http/Controllers/CholloController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Chollo;
+
+class CholloController extends Controller
+{
+    public function index()
+    {
+        // Obtener todos los chollos y pasarlos a la vista
+        $chollos = Chollo::all();
+        return view('chollos.index', compact('chollos'));
+    }
+
+    public function create()
+    {
+        // Mostrar el formulario de creación
+        return view('chollos.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        Chollo::create($request->all());
+
+        return redirect()->route('chollos.index')->with('success', 'Chollo creado exitosamente');
+    }
+
+    public function edit($id)
+    {
+        // Mostrar el formulario de edición con la información del chollo
+        $chollo = Chollo::findOrFail($id);
+        return view('chollos.edit', compact('chollo'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $chollo = Chollo::findOrFail($id);
+        $chollo->update($request->all());
+
+        return redirect()->route('chollos.index')->with('success', 'Chollo actualizado exitosamente');
+    }
+
+    public function destroy($id)
+    {
+        // Lógica para eliminar un chollo de la base de datos
+        $chollo = Chollo::findOrFail($id);
+        $chollo->delete();
+
+        return redirect()->route('chollos.index')->with('success', 'Chollo eliminado exitosamente');
+    }
+}
+
